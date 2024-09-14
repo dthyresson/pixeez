512e9c52a07b27add4c4d7e2697bd987dd61e1f2
David Thyresson <dthyresson@gmail.com>
2024-09-13 21:20:42 -0400

    fix

--- api/src/jobs/RemoveImageBackgroundJob/RemoveImageBackgroundJob.ts
+++ api/src/jobs/RemoveImageBackgroundJob/RemoveImageBackgroundJob.ts
@@ -22,39 +22,39 @@ export const RemoveImageBackgroundJob = jobs.createJob({
     if (!pic) {
       jobs.logger.error({ picId }, 'Pic not found')
       return
+
+      // send base64 data uri of the original image in the Pic to fal as imageUrl
+      jobs.logger.debug({ picId }, 'Pic to get data uri')
+      const picDataUri = await pic.withDataUri()
+      const result = await removeBackground({ imageUrl: picDataUri.original })
+
+      jobs.logger.debug({ picId }, 'Fal processing done!')
+
+      // get the url of the image without background from the fal result
+      const withoutBackground = result['image']?.['url']
+
+      if (!withoutBackground) {
+        jobs.logger.error({ picId }, 'Fal processing failed')
+        return
+      }
+
+      // update the Pic with the new image without background
+      await db.pic.update({
+        where: { id: picId },
+        data: {
+          withoutBackground,
+        },
+      })
+
+      jobs.logger.debug({ picId }, 'Pic updated!')
+
+      // send webhook to refresh album live query for pics
+      await executeGraphQLWebhook({
+        query: OnBackgroundRemovedWebhook,
+        inputVariables: { id: picId },
+      })
+
+      jobs.logger.info({ picId }, 'RemoveImageBackgroundJob done!')
     }
-
-    // send base64 data uri of the original image in the Pic to fal as imageUrl
-    jobs.logger.debug({ picId }, 'Pic to get data uri')
-    const picDataUri = await pic.withDataUri()
-    const result = await removeBackground({ imageUrl: picDataUri.original })
-
-    jobs.logger.debug({ picId }, 'Fal processing done!')
-
-    // get the url of the image without background from the fal result
-    const withoutBackground = result['image']?.['url']
-
-    if (!withoutBackground) {
-      jobs.logger.error({ picId }, 'Fal processing failed')
-      return
-    }
-
-    // update the Pic with the new image without background
-    await db.pic.update({
-      where: { id: picId },
-      data: {
-        withoutBackground,
-      },
-    })
-
-    jobs.logger.debug({ picId }, 'Pic updated!')
-
-    // send webhook to refresh album live query for pics
-    await executeGraphQLWebhook({
-      query: OnBackgroundRemovedWebhook,
-      inputVariables: { id: picId },
-    })
-
-    jobs.logger.info({ picId }, 'RemoveImageBackgroundJob done!')
   },
 })
