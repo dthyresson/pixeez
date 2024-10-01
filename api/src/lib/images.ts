bf870d04ec3654812f6b0a542d806e4c30a9ec28
David Thyresson <dthyresson@gmail.com>
2024-09-30 21:48:36 -0400

    getBase64DataUri

--- api/src/lib/images.ts
+++ api/src/lib/images.ts
@@ -1,9 +1,16 @@
+import { logger } from './logger'
 import { storage } from './storage'
-
 export const getBase64DataUri = async (filePath: string): Promise<string> => {
-  const data = await storage.readFile(filePath)
-  const arrayBuffer = await data.arrayBuffer()
-  const base64Data = Buffer.from(arrayBuffer).toString('base64')
-  const mimeType = data.type
-  return `data:${mimeType};base64,${base64Data}`
+  try {
+    const file = await storage.readFile(filePath)
+    const base64Data = Buffer.from(await file.arrayBuffer()).toString('base64')
+    const mimeType = file.type
+
+    const dataUri = `data:${mimeType};base64,${base64Data}`
+    // logger.debug({ dataUri }, 'dataUri')
+    return dataUri
+  } catch (error) {
+    logger.error({ error, filePath }, 'Error creating base64 data URI')
+    throw error
+  }
 }
